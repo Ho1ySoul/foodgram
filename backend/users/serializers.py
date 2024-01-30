74183e12b67abcb0920ed3ce58d17b1b6a44f0b6
Stas Dorokhov <imho1ysoul@gmail.com>
2024-01-30 08:58:30 +0300

    remove dublicate serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,24 +1,10 @@
-import base64
-
-from django.core.files.base import ContentFile
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from recipe.models import User, Recipe
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            img_format, img_str = data.split(';base64,')
-            ext = img_format.split('/')[-1]
-            data = ContentFile(base64.b64decode(img_str), name='img.' + ext)
-        return super().to_internal_value(data)
-
-
 class RecipeForSerializer(ModelSerializer):
-    image = Base64ImageField()
-
     class Meta:
         model = Recipe
         fields = ('name',
